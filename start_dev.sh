fa728983c9e969830d68f92ae7042c2b36063484
Francisco <jscriptcoder@gmail.com>
2021-03-28 22:44:33 +0200

    Combining starter

--- start_dev.sh
+++ start_dev.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.dev.yml up --build
+env="$1"
+
+if [ -z "$env" ]; then 
+    env="dev"
+fi
+
+docker-compose -f docker-compose.$env.yml up --build --remove-orphans
